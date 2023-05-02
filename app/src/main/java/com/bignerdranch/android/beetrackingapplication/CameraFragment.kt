5ab54d2e118fa5a003f7ee903266edaf0f6196e8
Grant <grant_d_lohman@aol.com>
2023-05-02 17:33:51 -0500

    camera fragment

--- app/src/main/java/com/bignerdranch/android/beetrackingapplication/CameraFragment.kt
+++ app/src/main/java/com/bignerdranch/android/beetrackingapplication/CameraFragment.kt
@@ -1,59 +1,165 @@
 package com.bignerdranch.android.beetrackingapplication
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ProgressBar
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
+import java.io.IOException
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.Date
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val TAG = "CAMERA_FRAGMENT"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CameraFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CameraFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var imageButton: ImageButton
+    private lateinit var uploadImageFab: FloatingActionButton
+    private lateinit var uploadProgressBar: ProgressBar
+    private lateinit var mainView: View
+
+    private var newPhotoPath: String? = null
+    private var visibleImagePath: String? = null
+    private var imageFilename: String? = null
+    private var photoUri: Uri? = null
+
+    private val NEW_PHOTO_PATH_KEY = "new photo path key"
+    private val VISIBLE_IMAGE_PATH_KEY = "visible image path key"
+
+    private val storage = Firebase.storage
+
+    private val cameraActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result -> handleImage(result)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        setContentView(R.layout.fragment_camera)
+
+        newPhotoPath = savedInstanceState?.getString(NEW_PHOTO_PATH_KEY)
+        visibleImagePath = savedInstanceState?.getString(VISIBLE_IMAGE_PATH_KEY)
+
+        mainView = findViewById(R.id.content)
+
+        uploadProgressBar = findViewById(R.id.upload_progress_bar)
+        uploadImageFab = findViewById(R.id.upload_image_button)
+
+        uploadImageFab.setOnClickListener {
+            uploadImage()
+        }
+
+        imageButton = findViewById(R.id.imageButton)
+        imageButton.setOnClickListener {
+            takePicture()
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+    private fun uploadImage() {
+        if (photoUri != null && imageFilename != null) {
+
+            uploadProgressBar.visibility = View.VISIBLE
+
+            val imageStorageRootReference = storage.reference
+            val imageCollectionReference = imageStorageRootReference.child("images")
+            val imageFileReference = imageCollectionReference.child(imageFilename!!)
+            imageFileReference.putFile(photoUri!!)
+                .addOnCompleteListener {
+                    Snackbar.make(mainView, "Image uploaded!", Snackbar.LENGTH_LONG).show()
+                    uploadProgressBar.visibility = View.GONE
+                }
+                .addOnFailureListener { error ->
+                    Snackbar.make(mainView, "Error uploading image", Snackbar.LENGTH_LONG).show()
+                    Log.e(TAG, "Error uploading image $imageFilename", error)
+                    uploadProgressBar.visibility = View.GONE
+                }
+        } else {
+            Snackbar.make(mainView, "Take a picture first!", Snackbar.LENGTH_LONG).show()
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CameraFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CameraFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(NEW_PHOTO_PATH_KEY, newPhotoPath)
+        outState.putString(VISIBLE_IMAGE_PATH_KEY, visibleImagePath)
+    }
+
+    private fun takePicture() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val (photoFile, photoFilePath) = createImageFile()
+        if (photoFile != null) {
+            newPhotoPath = photoFilePath
+            photoUri = FileProvider.getUriForFile(
+                this,
+                "com.example.android.collage.fileprovider",
+                photoFile
+            )
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            cameraActivityLauncher.launch(takePictureIntent)
+        }
+    }
+
+    private fun createImageFile(): Pair<File?, String?> {
+        try {
+            val dateTime = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            imageFilename = "COLLAGE_$dateTime"
+            val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            val file = File.createTempFile(imageFilename, ".jpg", storageDir)
+            val filePath = file.absolutePath
+            return file to filePath
+        } catch (ex: IOException) {
+            return null to null
+        }
+    }
+
+    private fun handleImage(result: ActivityResult) {
+        when (result.resultCode) {
+            RESULT_OK -> {
+                Log.d(TAG, "Result ok, user took picture, image at $newPhotoPath")
+                visibleImagePath = newPhotoPath
             }
+            RESULT_CANCELED -> {
+                Log.d(TAG, "Result cancelled, no photo taken")
+            }
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        Log.d(TAG, "on window focus changed $hasFocus visible image at $visibleImagePath")
+        if (hasFocus) {
+            visibleImagePath?.let { imagePath ->
+                loadImage(imageButton, imagePath) }
+        }
+    }
+
+    private fun loadImage(imageButton: ImageButton, imagePath: String) {
+        Picasso.get()
+            .load(File(imagePath))
+            .error(android.R.drawable.stat_notify_error)
+            .fit()
+            .centerCrop()
+            .into(imageButton, object: Callback {
+                override fun onSuccess() {
+                    Log.d(TAG, "Loaded image $imagePath")
+                }
+                override fun onError(e: Exception?) {
+                    Log.e(TAG, "Error loading image $imagePath", e)
+                }
+            })
     }
 }
