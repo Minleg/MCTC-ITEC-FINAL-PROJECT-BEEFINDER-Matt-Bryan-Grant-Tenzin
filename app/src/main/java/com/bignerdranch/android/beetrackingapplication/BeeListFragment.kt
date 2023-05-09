bce066f09a33f066411cdbe834994446ef46a01c
minleg <minleg@minleg.com>
2023-05-09 08:40:28 -0500

    shows image on the recycler view but still need to get the image from firebase

--- app/src/main/java/com/bignerdranch/android/beetrackingapplication/BeeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/beetrackingapplication/BeeListFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 /**
  * A simple [Fragment] subclass.
@@ -27,6 +29,8 @@ class BeeListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val recyclerView = inflater.inflate(R.layout.fragment_bee_list, container, false)
+        //val recyclerViewItem = recyclerView.findViewById<RecyclerView>(R.id.bee_list)
+        //val beeImageView = recyclerViewItem.findViewById<ImageView>(R.id.beeImageView)
         if (recyclerView !is RecyclerView) {
             throw java.lang.RuntimeException("BeeListFragment view should be a recycler view")
         }
@@ -39,6 +43,11 @@ class BeeListFragment : Fragment() {
         beeViewModel.latestBees.observe(requireActivity()) { beeList ->
             adapter.bees = beeList
             adapter.notifyDataSetChanged()
+
+//            // Assuming you have an image URL stored in each Bee object
+//            beeList.forEach { bee ->
+//                Picasso.get().load(bee.imagePath).into(beeImageView)
+//            }
         }
         return recyclerView
     }
--- app/src/main/java/com/bignerdranch/android/beetrackingapplication/BeeRecyclerViewAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/beetrackingapplication/BeeRecyclerViewAdapter.kt
@@ -1,19 +1,44 @@
 package com.bignerdranch.android.beetrackingapplication
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 
-class BeeRecyclerViewAdapter(var bees: List<Bee>) : RecyclerView.Adapter<BeeRecyclerViewAdapter.ViewHolder>() {
+class BeeRecyclerViewAdapter(var bees: List<Bee>) :
+    RecyclerView.Adapter<BeeRecyclerViewAdapter.ViewHolder>() {
 
+    val baseImagePath: String = "https://firebasestorage.googleapis.com/v0/b/beespotter-2c9ea.appspot.com/o/"
     inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+        var imageView: ImageView = view.findViewById(R.id.beeImageView)
         fun bind(bee: Bee) {
             view.findViewById<TextView>(R.id.date_spotted).text = "${bee.dateSpotted}"
-            Picasso.get().load(bee.imagePath).into(view.findViewById<ImageView>(R.id.imageView))
+            loadImage(bee.imagePath!!, imageView)
+//            Picasso.get()
+//                .load(baseImagePath + bee.imagePath)
+//                .placeholder(R.drawable.placeholder_image) // Add placeholder image resource
+//                .error(R.drawable.error_images) // Add error image resource
+//                .into(
+//                    imageView,
+//                    object : Callback {
+//                        override fun onSuccess() {
+//                            // Image loaded successfully
+//                        }
+//
+//                        override fun onError(e: Exception?) {
+//                            // Log the error
+//                            Log.e("Picasso", "Error loading image: ${e?.message}")
+//                        }
+//                    },
+//                )
         }
     }
 
@@ -29,9 +54,32 @@ class BeeRecyclerViewAdapter(var bees: List<Bee>) : RecyclerView.Adapter<BeeRecy
     override fun onBindViewHolder(holder: BeeRecyclerViewAdapter.ViewHolder, position: Int) {
         val bee = bees[position]
         holder.bind(bee)
+
+//        // Load the image using Picasso
+//        val imagePath = bee.imagePath
+//        if (imagePath != null) {
+//            val imageUrl = baseImagePath + imagePath
+//            val imageView = holder.itemView.findViewById<ImageView>(R.id.beeImageView)
+//
+//            Picasso.get()
+//                .load(imageUrl)
+//                .into(imageView)
+//        }
     }
 
     override fun getItemCount(): Int {
         return bees.size
+    }
+
+    fun loadImage(imagePath: String, imageView: ImageView) {
+        val storageRef = Firebase.storage.reference
+        val imageRef = storageRef.child(imagePath)
+
+        imageRef.downloadUrl.addOnSuccessListener { uri ->
+            Picasso.get().load(uri).into(imageView)
+        }.addOnFailureListener { exception ->
+            // Handle the error
+            Log.e("Adapter", "Error loading image: ${exception.message}")
+        }
     }
 }
