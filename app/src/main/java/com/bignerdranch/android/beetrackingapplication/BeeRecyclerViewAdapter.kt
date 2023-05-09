92b84814ba54e1e55baa3b48db24328ec46509ca
minleg <minleg@minleg.com>
2023-05-09 15:56:56 -0500

    adjust imaged size

--- app/src/main/java/com/bignerdranch/android/beetrackingapplication/BeeRecyclerViewAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/beetrackingapplication/BeeRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.beetrackingapplication
 
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -7,21 +8,26 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 class BeeRecyclerViewAdapter(var bees: List<Bee>) :
     RecyclerView.Adapter<BeeRecyclerViewAdapter.ViewHolder>() {
 
-    val baseImagePath: String = "https://firebasestorage.googleapis.com/v0/b/beespotter-2c9ea.appspot.com/o/"
+    val baseImagePath: String = "gs://beespotter-2c9ea.appspot.com"
+    private lateinit var context: Context
     inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
+        val storageReference = Firebase.storage.reference
         var imageView: ImageView = view.findViewById(R.id.beeImageView)
+
         fun bind(bee: Bee) {
             view.findViewById<TextView>(R.id.date_spotted).text = "${bee.dateSpotted}"
-            // loadImage(bee.imagePath!!, imageView)
+            Log.d("ImagePath", "$baseImagePath + ${bee.imagePath}")
             Picasso.get()
                 .load(baseImagePath + bee.imagePath)
                 .placeholder(R.drawable.placeholder_image) // Add placeholder image resource
@@ -39,6 +45,9 @@ class BeeRecyclerViewAdapter(var bees: List<Bee>) :
                         }
                     },
                 )
+//            Glide.with()
+//                .load(storageReference)
+//                .into(imageView)
         }
     }
 
@@ -54,32 +63,9 @@ class BeeRecyclerViewAdapter(var bees: List<Bee>) :
     override fun onBindViewHolder(holder: BeeRecyclerViewAdapter.ViewHolder, position: Int) {
         val bee = bees[position]
         holder.bind(bee)
-
-//        // Load the image using Picasso
-//        val imagePath = bee.imagePath
-//        if (imagePath != null) {
-//            val imageUrl = baseImagePath + imagePath
-//            val imageView = holder.itemView.findViewById<ImageView>(R.id.beeImageView)
-//
-//            Picasso.get()
-//                .load(imageUrl)
-//                .into(imageView)
-//        }
     }
 
     override fun getItemCount(): Int {
         return bees.size
     }
-
-//    fun loadImage(imagePath: String, imageView: ImageView) {
-//        val storageRef = Firebase.storage.reference
-//        val imageRef = storageRef.child(imagePath)
-//
-//        imageRef.downloadUrl.addOnSuccessListener { uri ->
-//            Picasso.get().load(uri).into(imageView)
-//        }.addOnFailureListener { exception ->
-//            // Handle the error
-//            Log.e("Adapter", "Error loading image: ${exception.message}")
-//        }
-//    }
 }
